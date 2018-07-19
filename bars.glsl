491bbd63ed81f968d7eda91b13dcad9b3a356c7e
hrqmonteiro <henriquemonteiro@windowslive.com>
2018-07-19 11:18:30 -0300

    Add files via upload

---
+++ bars.glsl
@@ -0,0 +1,21 @@
+
+/* center line thickness (pixels) */
+#define C_LINE 1
+
+/* width (in pixels) of each bar */
+#define BAR_WIDTH 4
+/* width (in pixels) of each bar gap */
+#define BAR_GAP 1
+/* outline color */
+#define BAR_OUTLINE #FFFFFF
+/* outline width (in pixels, set to 0 to disable outline drawing) */
+#define BAR_OUTLINE_WIDTH 1
+/* Amplify magnitude of the results each bar displays */
+#define AMPLIFY 120
+/* Bar color */
+#define COLOR (#FFFFFF * ((d / 60) + 1))
+/* Direction that the bars are facing, 0 for inward, 1 for outward */
+#define DIRECTION 0
+/* Whether to switch left/right audio buffers */
+#define INVERT 0
+
